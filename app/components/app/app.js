502f9f10cadfceeed947a9230e0924f766e20e64
html5css3-pro <html5css3.pro@gmail.com>
2018-01-03 12:35:57 +0300

    header + footer + temp(css/js)

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../header/index';
+import Footer from '../footer/index';
 
 class App extends Component {
 
@@ -7,6 +8,8 @@ class App extends Component {
     return (
       <main>
         <Header />
+				
+				<Footer />
       </main>
     );
   }
