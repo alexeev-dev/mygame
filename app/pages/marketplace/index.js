a07f18f23c60bf36533713e2bf08a63343e05403
html5css3-pro <html5css3.pro@gmail.com>
2018-01-12 18:07:47 +0300

    svoistva

--- app/pages/marketplace/index.js
+++ app/pages/marketplace/index.js
@@ -8,7 +8,7 @@ class MarketplacePage extends Component {
     return (
       <div id="content-block">
         <Hero />
-        <Item />
+        <Item gen={{head: 0, hair: 0, corn: 0, ears: 0, eyes: 0}} color={{head: 0, hair: 0}} />
       </div>
     );
   }
