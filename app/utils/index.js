374a93b9f282e3ff830a1e30f9cfca0cba774c80
Вова <vladimir-shalaev22@yandex.ru>
2018-01-16 20:03:56 +0700

    Небольшие правки

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,3 +1,4 @@
+import Web3 from 'web3'
 // Here's how we would access our contract:
 const ABI = [
 	{
@@ -71,12 +72,10 @@ const repeatString = (str, size) => {
   return result
 }
 
-if (typeof web3 !== 'undefined') {
-  web3 = new Web3(web3.currentProvider);
-} else {
-  // set the provider you want from Web3.providers
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:3333/"));
-}
+const web3 = (
+	typeof web3 !== 'undefined' ? new Web3(web3.currentProvider)
+		: new Web3(new Web3.providers.HttpProvider("http://localhost:3333/"))
+)
 
 class SmartUnicorn {
 	constructor() {
