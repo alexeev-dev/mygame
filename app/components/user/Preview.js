c2db08af6e5f9999bd9e89c8ff66cd041881a564
html5css3-pro <html5css3.pro@gmail.com>
2018-01-18 15:29:46 +0300

    Добавил все необходимые компоненты для выполнения тестов

--- app/components/user/Preview.js
+++ app/components/user/Preview.js
@@ -3,17 +3,20 @@ import React, { Component } from 'react';
 class UserPreview extends Component {
   render() {
     return (
-      <div className="be-user-block">
-        <div className="be-user-detail">
-          <a className="be-ava-user" href="#">
-            <img src="img/ava.png" alt="" />
-          </a>
-          <p className="be-use-name">Alex Alexeev</p>
-          <span className="be-user-info">
-            Singapore, Singapore
-          </span>
+      <div className="login-header-block">
+        <div className="login_block">
+          <div className="be-drop-down login-user-down">
+            <img className="login-user" src="img/login.jpg" alt=""/>
+            <span className="be-dropdown-content">Hi, <span>Alex</span></span>
+            <div className="drop-down-list a-list">
+              <a href="#">Invite</a>
+              <a href="#">Logout</a>
+            </div>
+          </div>
+          <a className="btn-login btn color-1 size-2 hover-2" href=""><i className="fa fa-user"></i>Log in</a>
         </div>
       </div>
+
     );
   }
 }
