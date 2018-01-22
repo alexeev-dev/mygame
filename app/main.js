7f8cb493b1b032fcc2773f37297c096d8a7b215f
Вова <vladimir-shalaev22@yandex.ru>
2018-01-22 21:23:41 +0700

    Добавляет Redux Dev ToolsЭ

--- app/main.js
+++ app/main.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 
 import App from './components/app/app';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk)
+))
 
 ReactDOM.render((
   <BrowserRouter>
