662d2ae9e59018c943e3c8bf2874a3bde8eb994f
html5css3-pro <html5css3.pro@gmail.com>
2018-01-23 10:30:39 +0300

    Удалены все jquery

--- app/pages/Hero.js
+++ app/pages/Hero.js
@@ -11,6 +11,10 @@ class Hero extends Component {
           <p>Do not miss the revolution that is happening right now with UnicornGO!</p>
 
           <div className="step">
+            <a className="be-register btn color-3 size-1 hover-6"><i className="fa fa-lock"></i>sign up now</a>
+          </div>
+
+          <div className="step">
             <div className="head-bg-video"><iframe width="560" height="315" src="https://www.youtube.com/embed/tfETpi-9ORs?rel=0" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></div>
             <a className="be-register btn color-1 size-1 hover-6"><i className="fa fa-cloud-download"></i>install metamask</a>
             <a className="be-register btn color-3 size-1 hover-6"><i className="fa fa-thumbs-up"></i>i had metamask</a>
