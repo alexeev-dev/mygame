6bd8875703597aab1a01d998b092e518cae66e08
Vladimir Shalaev <vladimir-shalaev22@yandex.ru>
2018-02-17 16:18:40 +0700

    Какая-то правка вроде

--- app/components/Unicorn/Comments.js
+++ app/components/Unicorn/Comments.js
@@ -2,46 +2,58 @@ import React, { Component } from 'react';
 
 import Comment from '../Unicorn/Comment'
 
-let textElem = {value: ''};
-
 class Comments extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  handleChange(e) {
+    this.setState({value: e.target.value})
+  }
+
   handleSubmit(e) {
-    e.preventDefault();
-    const btn = e.target;
-    btn.setAttribute('disabled', 'disabled');
-    setTimeout(() => {btn.removeAttribute('disabled')}, 1000);
+    e.preventDefault()
+    const btn = e.target
+    btn.setAttribute('disabled', 'disabled')
+    setTimeout(() => {btn.removeAttribute('disabled')}, 1000)
     try {
-      this.props.onSubmit(textElem.value);
-      textElem.value = '';
+      this.props.onSubmit(this.state.value)
+      this.setState({value: ''})
     } catch (e) {
       //console.log(e);
     }
   }
 
   render() {
+    const {items, price} = this.props
     return (
       <div className="be-comment-block">
-        <h1 className="comments-title">Comments ({this.props.items.length})</h1>
+        <h1 className="comments-title">Comments ({items.length})</h1>
         <p className="about-comment-block">
-          Price for comment is <a href="#" className="be-signup-link">{this.props.price} <i className="fa fa-btc"></i></a>
+          Price for comment is <a href="#" className="be-signup-link">{price} <i className="fa fa-btc"></i></a>
         </p>
         <form className="form-block">
           <div className="row">
             <div className="col-xs-12">
               <div className="form-group">
-                <textarea className="form-input" required="" placeholder="Your text" onChange={e => textElem = e.target}></textarea>
+                <textarea
+                  className="form-input"
+                  placeholder="Your text"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
               </div>
             </div>
             <a className="btn color-1 size-2 hover-1 pull-right" onClick={this.handleSubmit}>submit</a>
           </div>
         </form>
 
-        {this.props.items.map((item, i) => (
+        {items.map((item, i) => (
           <Comment key={(item.id || i)} comment={item} />
         ))}
 
